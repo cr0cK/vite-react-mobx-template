874e0a6b29edd04ee58264d4f0fde58a109f2843
Alexis Mineaud <alexis.mineaud@gmail.com>
2025-03-07 21:42:14 +0100

    Fix production build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,37 @@
-import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  server: {
-    host: '0.0.0.0'
-  },
+export default defineConfig(({ command, mode }) => {
+  return {
+    server: {
+      host: '0.0.0.0'
+    },
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
 
-  plugins: [TanStackRouterVite(), react(), tailwindcss()],
+    plugins: [TanStackRouterVite(), react(), tailwindcss()],
 
-  // https://theme-ui.com/guides/jsx-pragma#using-vite
-  esbuild: {
-    jsxFactory: 'jsx',
-    jsxInject: `import { jsx } from 'theme-ui'`
-  },
+    // https://theme-ui.com/guides/jsx-pragma#using-vite
+    esbuild:
+      mode === 'development'
+        ? {
+            jsxFactory: 'jsx',
+            jsxInject: `import { jsx } from 'theme-ui'`
+          }
+        : {},
 
-  build: {
-    minify: true,
-    emptyOutDir: true
-  },
+    build: {
+      minify: true,
+      emptyOutDir: true
+    },
 
-  clearScreen: false
+    clearScreen: false
+  }
 })
